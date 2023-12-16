f9bbe6c65fddc959f5721cde6c72e70f92515630
Drew Lee <sy3783@gmail.com>
2023-12-16 17:46:08 +0900

    💄 UI: change loader

--- src/components/shared/Loading.tsx
+++ src/components/shared/Loading.tsx
@@ -1,5 +1,9 @@
 const Loading = () => {
-  return <p className="p-4 w-full h-full text-center">Loading...</p>;
+  return (
+    <p className="p-4 w-full h-screen flex flex-col justify-center place-items-center">
+      <span className="loading loading-dots loading-lg"></span>
+    </p>
+  );
 };
 
 export default Loading;
