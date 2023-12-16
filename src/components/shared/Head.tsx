937fd0cba40e10c2a58089e38683bdbc85721b8a
Drew Lee <sy3783@gmail.com>
2023-12-16 17:39:47 +0900

    🔍 SEO: fix og img source

--- src/components/shared/Head.tsx
+++ src/components/shared/Head.tsx
@@ -12,18 +12,24 @@ export const Head = ({ title, description }: Props) => (
     <meta name="viewport" content="initial-scale=1.0, width=device-width" />
 
     <meta property="og:title" content={`${title} | Drew.log`} />
-    <meta property="og:description" content={description ?? "drew's blog"} />
+    <meta
+      property="og:description"
+      content={description ? description : "drew's blog"}
+    />
     <meta property="og:type" content="website" />
     <meta property="og:url" content="https://www.drewww.dev/" />
 
-    <meta property="og:image" content="/assets/og.png" />
+    <meta property="og:image" content="/og.png" />
     <meta property="og:image:width" content="800" />
     <meta property="og:image:height" content="400" />
 
     <meta name="twitter:card" content="summary_large_image" />
     <meta name="twitter:title" content="original" data-rh="true" />
-    <meta name="twitter:description" content={description ?? "drew's blog"} />
+    <meta
+      name="twitter:description"
+      content={description ? description : "drew's blog"}
+    />
     <meta name="twitter:site" content="https://www.drewww.dev/" />
-    <meta name="twitter:image" content="/assets/og.png" />
+    <meta name="twitter:image" content="/og.png" />
   </Helmet>
 );
