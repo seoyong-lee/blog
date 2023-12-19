53e1997f86e0888e9ed748c993431da0ae7a6cd9
Drew Lee <sy3783@gmail.com>
2023-12-19 14:02:39 +0900

    ✨ Feat: add archive placeholder

--- src/components/screens/Archive.tsx
+++ src/components/screens/Archive.tsx
@@ -1,5 +1,6 @@
 import { Head } from "~/components/shared/Head";
 import TitleHeader from "../shared/TitleHeader";
+import { Divider } from "react-daisyui";
 
 function PageArchive() {
   return (
@@ -7,6 +8,14 @@ function PageArchive() {
       <Head title={"Archive"}></Head>
       <div className="flex flex-col max-w-[780px] w-full h-screen px-6 sm:px-10 lg:py-0 pt-12">
         <TitleHeader title={"Archive"} />
+        <section className="px-2">
+          {/* <div className="mt-8 flex flex-col gap-4">
+            <span className="text-2xl font-bold tracking-tight">2023</span>
+            <span className="text-xl font-bold tracking-tight">December</span>
+            <div className="divider" />
+          </div> */}
+          <span className="font-bold tracking-tight">준비중입니다...</span>
+        </section>
       </div>
     </>
   );
