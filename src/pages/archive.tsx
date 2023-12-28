5a1e9495bba8ad3de5df0fed91e21b4cb657bc4d
Drew Lee <sy3783@gmail.com>
2023-12-28 17:04:02 +0900

    ✨ Feat: fix archive order

--- src/pages/archive.tsx
+++ src/pages/archive.tsx
@@ -53,55 +53,57 @@ function PageArchive() {
         <TitleHeader title={"Archive"} />
         <section className="px-2">
           {postsByDate ? (
-            Object.keys(postsByDate).map((year) => {
-              return (
-                <div className="mt-6 w-full flex flex-col gap-4" key={year}>
-                  <span className="text-2xl font-bold tracking-tight">
-                    {year}
-                    <span className="text-[14px] font-bold relative left-2 top-[-10px] opacity-60">
-                      {Object.keys(postsByDate[year]).length}
+            Object.keys(postsByDate)
+              .reverse()
+              .map((year) => {
+                return (
+                  <div className="mt-6 w-full flex flex-col gap-4" key={year}>
+                    <span className="text-2xl font-bold tracking-tight">
+                      {year}
+                      <span className="text-[14px] font-bold relative left-2 top-[-10px] opacity-60">
+                        {Object.keys(postsByDate[year]).length}
+                      </span>
                     </span>
-                  </span>
-                  {Object.keys(postsByDate[year])?.map((month) => {
-                    return (
-                      <Fragment key={year + month}>
-                        <div className="mt-2 flex justify-between flex-col md:flex-row w-full gap-10">
-                          <span className="text-xl font-bold tracking-tight w-[14rem]">
-                            {dayjs(month).format("MMMM")}
-                            <span className="text-[14px] font-bold relative left-2 top-[-7px] opacity-60">
-                              {postsByDate[year][month]?.length}
+                    {Object.keys(postsByDate[year])?.map((month) => {
+                      return (
+                        <Fragment key={year + month}>
+                          <div className="mt-2 flex justify-between flex-col md:flex-row w-full gap-10">
+                            <span className="text-xl font-bold tracking-tight w-[14rem]">
+                              {dayjs(month).format("MMMM")}
+                              <span className="text-[14px] font-bold relative left-2 top-[-7px] opacity-60">
+                                {postsByDate[year][month]?.length}
+                              </span>
                             </span>
-                          </span>
-                          <div className="w-full flex flex-col">
-                            {postsByDate[year][month].map((post) => {
-                              return (
-                                <div
-                                  className="mb-4 cursor-pointer"
-                                  key={post.id}
-                                  onClick={handleClickPost(post.id)}
-                                >
-                                  <h3 className="text-xl font-semibold">
-                                    {post?.title}
-                                  </h3>
-                                  <span className="text-sm font-medium opacity-60">
-                                    Date:{" "}
-                                    {dayjs(post?.createdAt).format(
-                                      "MMM DD, YYYY"
-                                    )}{" "}
-                                    | Author: {post?.author.name}
-                                  </span>
-                                </div>
-                              );
-                            })}
+                            <div className="w-full flex flex-col">
+                              {postsByDate[year][month].map((post) => {
+                                return (
+                                  <div
+                                    className="mb-4 cursor-pointer"
+                                    key={post.id}
+                                    onClick={handleClickPost(post.id)}
+                                  >
+                                    <h3 className="text-xl font-semibold">
+                                      {post?.title}
+                                    </h3>
+                                    <span className="text-sm font-medium opacity-60">
+                                      Date:{" "}
+                                      {dayjs(post?.createdAt).format(
+                                        "MMM DD, YYYY"
+                                      )}{" "}
+                                      | Author: {post?.author.name}
+                                    </span>
+                                  </div>
+                                );
+                              })}
+                            </div>
                           </div>
-                        </div>
-                        <div className="divider w-full my-0" />
-                      </Fragment>
-                    );
-                  })}
-                </div>
-              );
-            })
+                          <div className="divider w-full my-0" />
+                        </Fragment>
+                      );
+                    })}
+                  </div>
+                );
+              })
           ) : (
             <span className="font-bold tracking-tight">준비중입니다...</span>
           )}
