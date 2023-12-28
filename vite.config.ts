e9bfc16298542c2abedf9e15f442ffaba211b05c
Drew Lee <sy3783@gmail.com>
2023-12-28 12:25:03 +0900

    🚀 Deploy: add vercel plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
+import vercel from "vite-plugin-vercel";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     sourcemap: false,
   },
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), vercel(), tsconfigPaths()],
 });
