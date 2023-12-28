b48f37d845ea14f1d9c1b5632b215facf35b1239
Drew Lee <sy3783@gmail.com>
2023-12-28 12:32:24 +0900

    🚀 Deploy: fix route

--- vite.config.ts
+++ vite.config.ts
@@ -10,8 +10,6 @@ export default defineConfig({
   },
   plugins: [react(), vercel(), tsconfigPaths()],
   vercel: {
-    config: {
-      routes: [{ src: "/[^.]+", dest: "/" }],
-    },
+    rewrites: [{ source: "/[^.]+", destination: "/" }],
   },
 });
