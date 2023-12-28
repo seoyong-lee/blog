dc0931ae0e4cbb3bc1565f9624aff36331f2ebd0
Drew Lee <sy3783@gmail.com>
2023-12-28 13:14:26 +0900

    🚀 Deploy: revedrt

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
-import vercel from "vite-plugin-vercel";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     sourcemap: false,
   },
-  plugins: [react(), vercel(), tsconfigPaths()],
-  vercel: {
-    rewrites: [{ source: "/", destination: "/index.html" }],
-  },
+  plugins: [react(), tsconfigPaths()],
 });
